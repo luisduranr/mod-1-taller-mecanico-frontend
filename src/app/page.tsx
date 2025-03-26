d88911091003fdf14bd53299f8df89ae099cc064
luis.duran <luis.duran@fiscalia.gob.bo>
2025-03-26 01:47:13 -0400

    se agrego boton

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,11 @@
+import { Button } from "@/components/ui/button";
+
  function HomePage() {
   return (
     <div>
       <h1>Page</h1>
       <p>Page content</p>
+      <Button>Click me</Button>
     </div>
   );
 }
